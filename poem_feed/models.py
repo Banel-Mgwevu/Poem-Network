667b03f11a504d6f1d8288d6ffd1c838a88430b2
Banel Mgwevu <71422029+Banel-Mgwevu@users.noreply.github.com>
2022-06-20 14:51:19 +0200

    New Changes, Added Bitcoin Intergration and added database

--- poem_feed/models.py
+++ poem_feed/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
+
+
+class feed(models.Model):
+    author=models.CharField(max_length=50)
+    title=models.CharField(max_length=100)
+    msg=models.TextField(max_length=600)
+    date_created = models.DateTimeField(default=now, editable=False)
+
 
 # Create your models here.
